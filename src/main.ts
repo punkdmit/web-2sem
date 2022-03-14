3490e6b462ea2c8389ae5eabf05c578d0cae39ed
punkdmit <71826735+punkdmit@users.noreply.github.com>
2022-03-14 11:50:13 +0300

    отчет

--- src/main.ts
+++ src/main.ts
@@ -10,10 +10,10 @@ const PORT = process.env.PORT || 3000;
 console.log(process.env);
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
-  app.useStaticAssets(join(__dirname, '..', 'public'));
-  app.setBaseViewsDir(join(__dirname, '..', 'views'));
-  app.setViewEngine('hbs');
+  app.useStaticAssets(join(__dirname, '..', 'public')); //обслуживание статических файлов
+  app.setBaseViewsDir(join(__dirname, '..', 'views')); //Сообщим приложению, где искать наши views
+  app.setViewEngine('hbs'); //механизм шаблонов для отображения
 
-  await app.listen(PORT);
+  await app.listen(PORT); //создает прослушиватель на указанном порту или пути
 }
 bootstrap();
